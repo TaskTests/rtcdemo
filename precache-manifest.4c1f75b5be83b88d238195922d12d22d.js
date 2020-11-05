6c77ef303e96f7658b1b3546e220e8f384f46c2e
Jabin <mohdjabintgi@gmail.com>
2020-11-05 22:24:04 +0530

    Updates

---
+++ precache-manifest.4c1f75b5be83b88d238195922d12d22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b49feb3b9f38640a9c71f49224ef67b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a50de1fd2cb86360f78e",
+    "url": "/static/css/main.9c5696ac.chunk.css"
+  },
+  {
+    "revision": "1ad8f4bde51c5755ec7e",
+    "url": "/static/js/2.f85b82ac.chunk.js"
+  },
+  {
+    "revision": "1c538bda74831820fcc36388b7d77cd1",
+    "url": "/static/js/2.f85b82ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50de1fd2cb86360f78e",
+    "url": "/static/js/main.2b6bb82d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
