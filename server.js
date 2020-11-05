53325a7599f198e855d65af89baac4bb89f9c91b
Jabin <mohdjabintgi@gmail.com>
2020-11-05 21:49:15 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,66 +1,66 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socket = require('socket.io');
+const socket = require("socket.io");
 const io = socket(server);
-const path = require('path');
+const path = require("path");
 const rooms = {};
 
-io.on('connection', socket => {
-    socket.on("create room", roomId => {
-        socket.type = "host";
-        socket.roomId = roomId;
-        socket.join(roomId);
-        rooms[roomId] = socket.id;
-    })
+io.on("connection", (socket) => {
+  socket.on("create room", (roomId) => {
+    socket.type = "host";
+    socket.roomId = roomId;
+    socket.join(roomId);
+    rooms[roomId] = socket.id;
+  });
 
-    socket.on('join room', roomId => {
-        if (rooms[roomId]) {
-            socket.join(roomId);
-            socket.roomId = roomId;
-            socket.emit("hostId",rooms[roomId]);
-        } else {
-            socket.emit('no room');
-        }
-    })
+  socket.on("join room", (roomId) => {
+    if (rooms[roomId]) {
+      socket.join(roomId);
+      socket.roomId = roomId;
+      socket.emit("hostId", rooms[roomId]);
+    } else {
+      socket.emit("no room");
+    }
+  });
 
-    socket.on("offer",data=>{
-        data.from = socket.id;
-        console.log("Offer");
-        io.to(data.to).emit("offer",data);
-    })
-    socket.on("answer",data=>{
-        data.from = socket.id;
-        console.log("answer");
-        io.to(data.to).emit("answer",data);
-    })
+  socket.on("offer", (data) => {
+    data.from = socket.id;
+    console.log("Offer");
+    io.to(data.to).emit("offer", data);
+  });
+  socket.on("answer", (data) => {
+    data.from = socket.id;
+    console.log("answer");
+    io.to(data.to).emit("answer", data);
+  });
 
-    socket.on('ice-candidate',data=>{
-        data.from = socket.id;
-        io.to(data.to).emit('ice-candidate',{val:data.candidate,userID:socket.id})
-    })
+  socket.on("ice-candidate", (data) => {
+    data.from = socket.id;
+    io.to(data.to).emit("ice-candidate", {
+      val: data.candidate,
+      userID: socket.id,
+    });
+  });
 
-    socket.on('disconnect', () => {
-        if (socket.roomId) {
-            if (socket.type == "host") {
-                io.to(socket.roomId).emit("host left");
-                rooms[socket.roomId] = null;
-            } else {
-                io.to(socket.roomId).emit("user left", socket.id);
-            }
-        }
-    })
-})
+  socket.on("disconnect", () => {
+    if (socket.roomId) {
+      if (socket.type == "host") {
+        io.to(socket.roomId).emit("host left");
+        rooms[socket.roomId] = null;
+      } else {
+        io.to(socket.roomId).emit("user left", socket.id);
+      }
+    }
+  });
+});
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-if(process.env.PROD){
-    app.use(express.static(path.join(__dirname,'./client/build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'./client/build/index.html'))
-    })
-}    
-
-const port =  process.env.PORT || 8000;
-server.listen(port, () => console.log(`Server running on ${port}`))
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log(`Server running on ${port}`));
